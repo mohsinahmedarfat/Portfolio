91324ad5fd02cb4ec8b0eaf815a593003832f8d5
rfaraf <mohsinarfat53@gamil.com>
2024-07-14 17:57:15 +0600

    modified: skill icons

--- src/Compos/Technologies.jsx
+++ src/Compos/Technologies.jsx
@@ -1,28 +1,64 @@
+import jwt from "../assets/jwt.svg";
+import Marquee from "react-fast-marquee";
+
 const Technologies = () => {
-  const skills = [
-    "HTML5",
-    "CSS3",
-    "Tailwind CSS",
-    "JavaScript",
-    "React",
-    "React Router",
-    "Node.js",
-    "Express.js",
-    "MongoDB",
-  ];
   return (
     <section>
-      <h2 className="text-3xl font-bold">Technologies I&apos;ve used</h2>
-      <div className="mt-8 space-x-4 text-center">
-        {skills.map((skill, index) => (
-          <span
-            key={index}
-            className="inline-block mb-5 bg-[#C3BEF0] px-4 py-2 rounded"
-          >
-            {skill}
-          </span>
-        ))}
-      </div>
+      <h2 className="text-3xl font-bold mb-10">Technologies I&apos;ve used</h2>
+
+      {/* <Marquee>
+        I can be a React component, multiple React components, or just some
+        text.
+      </Marquee> */}
+      <Marquee pauseOnHover>
+        <div className="flex gap-10 overflow-x-auto">
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg"
+          />
+
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg"
+          />
+
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+          />
+
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+          />
+
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+          />
+
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg"
+          />
+
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
+          />
+
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"
+          />
+
+          <img
+            className="size-20"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
+          />
+          <img className="size-20 mr-10" src={jwt} />
+        </div>
+      </Marquee>
     </section>
   );
 };
